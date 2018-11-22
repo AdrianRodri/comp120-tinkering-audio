320a16c260dc20060e1477481e875a7793afda88
AdrianRodri <rodriguez.adrian@protonmail.com>
2018-11-22 15:45:35 +0000

    Updated note selection randomizer in main.py

--- Audio Contract/main.py
+++ Audio Contract/main.py
@@ -41,33 +41,42 @@ print(starting_note)
 current_pos = keys.index(starting_note)
 next_pos = current_pos + random.randint(-1,1)
 
+
 if next_pos > 0 or next_pos < len(keys):
     second_note = keys[next_pos]
 else :
-    current_pos = next_pos
+    step = random.randint(-1, 1)
+    next_pos = current_pos + 2 * (-step)
 print(second_note)
 
-next_pos = current_pos + random.randint(-1,1)
+
 if next_pos > 0 or next_pos < len(keys):
     third_note = keys[next_pos]
 else :
-    current_pos = next_pos
+    step = random.randint(-1, 1)
+    next_pos = current_pos + 2 * (-step)
+    third_note = next_pos
 print(second_note)
+
 
 next_pos = current_pos + random.randint(-1,1)
 if next_pos > 0 or next_pos < len(keys):
-    fourth_note = keys[next_pos]
+    fourth_note_note = keys[next_pos]
 else :
-    current_pos = next_pos
-print(second_note)
+    step = random.randint(-1, 1)
+    next_pos = current_pos + 2 * (-step)
+    fourth_note = next_pos
+print(fourth_note)
+
 
 next_pos = current_pos + random.randint(-1,1)
 if next_pos > 0 or next_pos < len(keys):
     fifth_note = keys[next_pos]
 else :
-    current_pos = next_pos
-print(second_note)
-
+    step = random.randint(-1, 1)
+    next_pos = current_pos + 2 * (-step)
+    fifth_note = next_pos
+print(fifth_note)
 
 
 '''
